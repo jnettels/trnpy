a9986e76ff07a5011376664d65278a0cfd78a0b9
Joris Nettelstroth <joris.nettelstroth@stw.de>
2018-01-28 00:58:46 +0100

    Add setup.py, which allows creation of a Windows executable

---
+++ setup.py
@@ -0,0 +1,23 @@
+'''
+Run with the following command prompt to create a Windows executable:
+python build_exe.py build
+'''
+from setuptools_scm import get_version
+from cx_Freeze import setup, Executable
+import os
+
+version = get_version(version_scheme='post-release')
+print('Building TRNpy with version tag: ' + version)
+
+# These settings solved an error, but the paths are different for every user:
+os.environ['TCL_LIBRARY'] = r'C:\Users\nettelstroth\Anaconda3\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\nettelstroth\Anaconda3\tcl\tk8.6'
+
+# The setup function
+setup(
+    name='TRNpy',
+    options={'build_exe': {'packages': ['idna', 'numpy']}},
+    version=version,
+    description='Parallelized TRNSYS simulation with Python',
+    executables=[Executable('trnpy.py', base=None)],
+)
