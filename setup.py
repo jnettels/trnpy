65cf4592f649b51b86e1b84d77a177af67f3ebdf
Joris Nettelstroth <joris.nettelstroth@stw.de>
2018-02-06 14:26:33 +0100

    Deal with development version numbers

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 '''
+Setup script for the TRNpy project.
+
 Run with the following command prompt to create a Windows executable:
-python build_exe.py build
+python setup.py build
 '''
 from setuptools_scm import get_version
 from cx_Freeze import setup, Executable
 import os
 
 version = get_version(version_scheme='post-release')
-if 'g' in version:  # Version does not fit to Windows' version scheme
-    version = '0.0.0.1'  # Use this to mark as a dev build
+
+if 'g' in version:  # 'Dirty' version, does not fit to Windows' version scheme
+    version_list = []
+    for i in version.split('.'):
+        try:  # Sort out all parts of the version name that are not integers
+            version_list.append(str(int(i)))
+        except Exception:
+            pass
+    if len(version_list) < 4:  # Windows version has a maximum length
+        version_list.append('1')  # Use this to mark as a dev build
+    version = '.'.join(version_list)
+
 print('Building TRNpy with version tag: ' + version)
 
 # These settings solved an error, but the paths are different for every user:
