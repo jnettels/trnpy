8c3ffe8b6f5c59ebc78aefe2ef081fde99948e77
Joris Nettelstroth <joris.nettelstroth@stw.de>
2019-04-04 01:08:54 +0200

    Setup installation: Create a new, seperate setup script for Python installation

---
+++ setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+'''
+TRNpy: Parallelized TRNSYS simulation with Python
+=================================================
+
+**Setup script for the TRNpy project**
+
+Run with the following command prompt to install into Python environment:
+
+.. code::
+
+    python setup.py install
+
+
+'''
+from setuptools import setup
+from setuptools_scm import get_version
+
+
+try:
+    version = get_version(version_scheme='post-release')
+except LookupError:
+    version = '0.0.0'
+    print('Warning: setuptools-scm requires an intact git repository to detect'
+          ' the version number for this build.')
+
+print('Building TRNpy with version tag: ' + version)
+
+# The setup function
+setup(
+    name='trnpy',
+    version=version,
+    description='Parallelized TRNSYS simulation with Python',
+    long_description=open('README.md').read(),
+    license='GPL-3.0',
+    author='Joris Nettelstroth',
+    author_email='joris.nettelstroth@stw.de',
+    url='https://bitbucket.org/joris_nettelstroth/trnpy',
+    install_requires=['pandas>=0.24.1', ],
+    python_requires='>=3.7',
+    scripts=['trnpy-script.py', ],
+    packages=['trnpy', ],
+)
