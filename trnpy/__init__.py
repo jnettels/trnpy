24d4e8ce08f87049e752b7fa3422852b6c3523db
Joris Nettelstroth <joris.nettelstroth@stw.de>
2023-06-06 09:40:58 +0200

    Print exceptions in __init__

--- trnpy/__init__.py
+++ trnpy/__init__.py
@@ -1,14 +1,16 @@
-"""Initialize the trnpy package and define version."""
+"""Initialize the package and define version."""
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
-    # Change here if project is renamed and does not equal the package name
     dist_name = 'trnpy'
+    # Try to get the version name from the installed package
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
     try:
+        # If package is not installed, try to get version from git
         from setuptools_scm import get_version
         __version__ = get_version(version_scheme='post-release',
                                   root='..', relative_to=__file__)
-    except Exception:
-        __version__ = 'unknown'
+    except (LookupError, Exception) as e:
+        print(e)
+        __version__ = '0.0.0'
